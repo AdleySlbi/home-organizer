4628fcf1a4e9acec6e9d82b37544cdec76a555cb
AdSlb <adley.salabi@gmail.com>
2022-02-15 18:40:49 +0100

    feat(manifesto): finished

--- manifesto.php
+++ manifesto.php
@@ -5,13 +5,13 @@
         <span class="subtitle">En France</span>
         <p>Le "Death cleaning" consiste à retirer les objets dont vous n'avez plus besoin ou que vous ne voulez plus dans votre maison pendant que vous le pouvez, pour épargner à quelqu'un d'autre la tâche de s'en occuper lorsque vous êtes parti. 
         <br>    
-        Le Death cleaning c’est quoi (lien vers la page « Death cleaning – voir texte plus bas)
         <br>
         Ensemble, entreprenons de trier, désencombrer, votre habitation ou votre bureau. Déterminons ce que vous souhaitez garder et ce qui doit sortir de votre intérieur. Réorganisons et mettons en place des solutions de rangement personnalisées. Je partage avec vous mes techniques et ma méthode pour que le rangement soit pérenne quelles que soient les pièces de votre habitation.</p>
-        <div>
+        
+        <div class="cta">
             <!-- Créer un lien vers la réalisation d'un devis -->
-            <span>Forfait 3h - 6h</span>
-            <span>Tarif sur Devis</span>
+            <span><b>Forfait :</b> 3h - 6h</span>
+            <span><i>Tarif sur Devis<i></span>
         </div>
         
         <!-- <div class="wrapper_manifest">
