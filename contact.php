4c4e8420a983d67d80155aaa7d4705d8b65222a0
AdSlb <adley.salabi@gmail.com>
2022-02-16 15:17:58 +0100

    feat(efzen) putain ça a marché

--- contact.php
+++ contact.php
@@ -1,17 +1,33 @@
 <section class="section_content section_contact" id="contact">
     <div class="wrapper_content">
         <h1>Me contacter</h1>
-        <p>
+        <p class="subtitle">
             Je suis à vos côtés pour anticiper, organiser, accompagner, pour que le changement soit le plus serein possible.
         </p>
-        <h2>Pour ces prestations contactez moi</h2>
-        <div>
-            <a href="@mailto:contact@homeorganiser.eu">contact@homeorganiser.eu</a>
-            <a href="@telto:+33622004397">+33(0)6.00.43.97</a>
-            <p>Entretien téléphonique : </p>
-            Quel que soit votre projet, je vous propose de commencer par un échange téléphonique pour faire le point sur vos objectifs. Suivi d'une "Visite conseil".
+        <div class="wrapper-contact">
+            <img src="./img/canape.jpg" alt="Image d'un canapé">
+            <div class="wrapper-info">
+                <h2>Pour ces prestations, <br> contactez moi</h2>
+                <a href="@mailto:contact@homeorganiser.eu" class="link">
+                <i class="fa-solid fa-envelope-open-text"></i>      
+                Mail : contact@homeorganiser.eu
+                </a>
+                <a href="@telto:+33622004397" class="link">
+                <i class="fa-solid fa-phone"></i>  
+                Téléphone : +33(0)6.00.43.97
+                </a>
+                <h3>
+                    Entretien téléphonique
+                </h3>
+                <p>
+                    Quel que soit votre projet, je vous propose de commencer par un échange téléphonique pour faire le point sur vos objectifs. Suivi d'une "Visite conseil".
+                </p>      
+                <p>
+                    <i class="subtitle">Intenrvention sur Paris et dans toutes les régions en France</i>
+                </p>
+            </div>
         </div>
-        <span><i>Intenrvention sur Paris et dans toutes les régions en France</i></span>
+        
 
 <!-- 
         <div class="contact_wrapper_map">
